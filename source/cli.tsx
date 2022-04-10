247cfbecc46f44847ec58a69d4e21e5de768569b
John Ruble <john.ruble@atomicobject.com>
2022-04-09 23:11:44 -0400

    npx create-ink-app --typescript

---
+++ source/cli.tsx
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+import React from 'react';
+import {render} from 'ink';
+import meow from 'meow';
+import App from './ui';
+
+const cli = meow(`
+	Usage
+	  $ inkle
+
+	Options
+		--name  Your name
+
+	Examples
+	  $ inkle --name=Jane
+	  Hello, Jane
+`, {
+	flags: {
+		name: {
+			type: 'string'
+		}
+	}
+});
+
+render(<App name={cli.flags.name}/>);
