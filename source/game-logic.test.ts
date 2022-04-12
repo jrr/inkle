0a8f80c7e6731a0743ebb9cd477701d62cd83f7f
John Ruble <john.ruble@atomicobject.com>
2022-04-11 21:20:23 -0400

    add more unit tests, including failing one for two-letter case

--- source/game-logic.test.ts
+++ source/game-logic.test.ts
@@ -3,10 +3,10 @@ import { colorGuess } from "./game-logic";
 function expectEqual<T>(actual: T, expected: T) {
   expect(expected).toStrictEqual(actual);
 }
+
 describe("colorGuess", () => {
   it("colors green", () => {
     const result = colorGuess("ABCDE", "ABCDE");
-    expect(result).toBeTruthy();
     expectEqual(result, {
       letters: [
         { color: "green", letter: "A" },
@@ -14,6 +14,98 @@ describe("colorGuess", () => {
         { color: "green", letter: "C" },
         { color: "green", letter: "D" },
         { color: "green", letter: "E" },
+      ],
+    });
+  });
+
+  it("colors yellow", () => {
+    const result = colorGuess("EABCD", "ABCDE");
+    expectEqual(result, {
+      letters: [
+        { color: "yellow", letter: "A" },
+        { color: "yellow", letter: "B" },
+        { color: "yellow", letter: "C" },
+        { color: "yellow", letter: "D" },
+        { color: "yellow", letter: "E" },
+      ],
+    });
+  });
+
+  it("colors gray", () => {
+    const result = colorGuess("JKLMN", "ABCDE");
+    expectEqual(result, {
+      letters: [
+        { color: "gray", letter: "A" },
+        { color: "gray", letter: "B" },
+        { color: "gray", letter: "C" },
+        { color: "gray", letter: "D" },
+        { color: "gray", letter: "E" },
+      ],
+    });
+  });
+
+  it("colors gray", () => {
+    const result = colorGuess("JKLMN", "ABCDE");
+    expectEqual(result, {
+      letters: [
+        { color: "gray", letter: "A" },
+        { color: "gray", letter: "B" },
+        { color: "gray", letter: "C" },
+        { color: "gray", letter: "D" },
+        { color: "gray", letter: "E" },
+      ],
+    });
+  });
+
+  it("handles a mixture", () => {
+    const result = colorGuess("FEWER", "RIPEN");
+    expectEqual(result, {
+      letters: [
+        { color: "yellow", letter: "R" },
+        { color: "gray", letter: "I" },
+        { color: "gray", letter: "P" },
+        { color: "green", letter: "E" },
+        { color: "gray", letter: "N" },
+      ],
+    });
+  });
+
+  it("handles a simple two-letter case", () => {
+    const result = colorGuess("FEWER", "STEER");
+    expectEqual(result, {
+      letters: [
+        { color: "gray", letter: "S" },
+        { color: "gray", letter: "T" },
+        { color: "yellow", letter: "E" },
+        { color: "green", letter: "E" },
+        { color: "green", letter: "R" },
+      ],
+    });
+  });
+
+  it("handles two-letter case - two yellows", () => {
+    const result = colorGuess("TWIST", "OTTER");
+    expectEqual(result, {
+      letters: [
+        { color: "gray", letter: "O" },
+        { color: "yellow", letter: "T" },
+        { color: "yellow", letter: "T" },
+        { color: "gray", letter: "E" },
+        { color: "gray", letter: "R" },
+      ],
+    });
+  });
+
+  it("handles two-letter case - yellow+gray", () => {
+    // https://nerdschalk.com/wordle-same-letter-twice-rules-explained-how-does-it-work/
+    const result = colorGuess("ABBEY", "KEEPS");
+    expectEqual(result, {
+      letters: [
+        { color: "gray", letter: "K" },
+        { color: "yellow", letter: "E" },
+        { color: "gray", letter: "E" },
+        { color: "gray", letter: "P" },
+        { color: "gray", letter: "S" },
       ],
     });
   });
