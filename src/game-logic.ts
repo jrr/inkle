7c9f7165c1db996fec8426544174b960f7650c7b
John Ruble <john.ruble@atomicobject.com>
2022-04-13 22:08:28 -0400

    move util

--- src/game-logic.ts
+++ src/game-logic.ts
@@ -1,13 +1,9 @@
 import itiriri from "itiriri";
 import { WORD_LEN } from "./constants";
 import { GuessedLetter, GuessedRow } from "./types";
+import { notEmpty } from "./util";
 import { possibleSolutions } from "./words/possible-solutions";
 import { validWords } from "./words/valid-words";
-
-// https://stackoverflow.com/a/46700791
-function notEmpty<TValue>(value: TValue | null | undefined): value is TValue {
-  return value !== null && value !== undefined;
-}
 
 // todo: reimplement better version. recursion? reduce?
 export function colorGuess(solution: string, currentRow: string): GuessedRow {
--- src/util.ts
+++ src/util.ts
@@ -1,1 +1,8 @@
 export const spaceString = (s: string) => [...s].join(" ");
+
+// https://stackoverflow.com/a/46700791
+export function notEmpty<TValue>(
+  value: TValue | null | undefined
+): value is TValue {
+  return value !== null && value !== undefined;
+}
