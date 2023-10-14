aee2db7d0eb21694dccfbf0ef2e276066a6f1c6c
John Ruble <john.ruble@atomicobject.com>
2023-10-13 21:31:01 -0400

    jest -> vitest, code changes

--- src/game-logic.test.ts
+++ src/game-logic.test.ts
@@ -1,3 +1,4 @@
+import { describe, it, expect } from "vitest";
 import { expectEqual } from "./test-util.js";
 import { colorGuess } from "./game-logic.js";
 
--- src/test-util.ts
+++ src/test-util.ts
@@ -1,3 +1,4 @@
+import { expect } from "vitest";
 export function expectEqual<T>(actual: T, expected: T) {
   expect(expected).toStrictEqual(actual);
 }
